96d8c095e1e1f82bd2057360a0590d70c1c83a92
Matthew Keitelman <zamattiac@gmail.com>
2017-06-01 10:07:17 -0400

    Add docker_install script

---
+++ docker_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Installs Docker CE 17.03.1 for CentOS
+sudo yum remove docker \
+                  docker-common \
+                  container-selinux \
+                  docker-selinux \
+                  docker-engine
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum makecache fast
+sudo yum -y install docker-ce-17.03.1.ce-1.el7.centos
+sudo service docker start
+echo -e "\033[0;31mRan 'service docker start' for you to start Docker daemon \033[0m"
+
